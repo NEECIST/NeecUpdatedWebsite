2d63f56cfbcc61d274f0764c79519d8d509766e8
Andre-MR-Pereira <54562621+Andre-MR-Pereira@users.noreply.github.com>
2022-03-18 18:58:51 +0000

    Update firebase.js

--- site/src/firebase.js
+++ site/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 let config = {
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY, //FIXME esconder
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
     authDomain: "neec-chaves.firebaseapp.com",
     databaseURL: "https://neec-chaves-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "neec-chaves",
     storageBucket: "neec-chaves.appspot.com",
     messagingSenderId: "823950398509",
-    appId: process.env.VUE_APP_FIREBASE_APP_ID, //FIXME esconder
-    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID //FIXME esconder
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 firebase.initializeApp(config);
 export default firebase.database();
