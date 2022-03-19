c9ccee608255c2290bd93555a948f0f45ed13715
Andre-MR-Pereira <andregdpereira@gmail.com>
2022-03-19 18:54:34 +0000

    small fix

--- site/vue.config.js
+++ site/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  VUE_APP_OPORTUNITIES_LINK: process.env.VUE_APP_OPORTUNITIES_LINK
+  transpileDependencies: true
 })
