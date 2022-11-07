2ead1178ff49ca8b09cb8a7a3161810aeed4d42f
Midas <goncalomidoes@tecnico.ulisboa.pt>
2022-11-07 09:43:45 +0000

    Updated Redirects

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -20,9 +20,9 @@ const routes = [
         name: 'Team',
         component: Team
     },
-    { 
-        path: '/404', 
-        component: Error_Handling 
+    {
+        path: '/404',
+        component: Error_Handling
     },
     {
         path: '/oportunities',
@@ -38,16 +38,20 @@ const routes = [
         path: '/form',
         name: 'Form',
         component: Form
-    },{ 
-        path: '/certificate', 
-        component: Error_Handling 
+    }, {
+        path: '/certificate',
+        component: Error_Handling
     }
-    ,{
+    , {
         path: '/certificate/:id',
         name: 'Certificate',
         component: Certificate
     }
-    
+    , {
+        path: '/redirect/drive',
+        redirect: 'https://drive.google.com/open?id=1HjuUvki3FrK9BKkcequsfCHxNjIVtWbz'
+    }
+
 ]
 
 const router = createRouter({
