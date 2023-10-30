3596c4a09bf96dd92de1c1c3adf8562b08ffb129
Midas <goncalomidoes@tecnico.ulisboa.pt>
2023-10-30 16:46:35 +0000

    Upgraded Certificates

--- site/src/pages/Certificate.vue
+++ site/src/pages/Certificate.vue
@@ -1,79 +1,127 @@
 <template>
-    <div class="certificate-app">
-        <div v-if="initialized">
-<h2>Certificado de Participação</h2>
-<div>
-    <img :src='certificateUrl' alt="certificate Image">
-</div>
-</div>
+  <div class="certificate-app">
+    <div v-if="initialized">
+      <h2>Certificado de Participação</h2>
+      <div>
+        <img :src="certificateUrl" alt="certificate Image" />
+      </div>
+    </div>
     <div v-else>
       <PulseLoader :color="'#009DE0'"></PulseLoader>
     </div>
-        
-        <div>{{this.error}}</div>
-    </div>
+
+    <div>{{ this.error }}</div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
-
+import axios from "axios";
+//import { url } from 'inspector';
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import { createCanvas } from "canvas";
 export default {
   name: "certificate-page",
   components: {
-    PulseLoader
-},
+    PulseLoader,
+  },
   data() {
-      return {
-        initialized: false,
-        error: null,
-        certificateUrl: null
-      }
-  },created(){
-    console.log("https://api.trello.com/1/cards/" +this.$route.params.id +"?attachments=true")
-        axios.get("https://api.trello.com/1/cards/" +this.$route.params.id +"?attachments=true")
-        .then(response => {
-            console.log(response.data)
-            this.certificateUrl = response.data.attachments[0].url
-        }).catch (() => {
-            this.error = "Unable to find certificate"
-        }).finally(()=>{
-          this.initialized = true
-        });
-    }
+    return {
+      initialized: false,
+      error: null,
+      certificateUrl: null,
+    };
+  },
+  created() {
+    const config = {
+      headers: {
+        Authorization: `Bearer 6d91366b1570f57e18431d3581ed78afe6bb64d7fde38be4b3df3f08fd4b8d83184344a85b0a49925c3a957d58e9764d8ae4bdc2fcdc5952fea7948ba3eaf724488648a354dfd7ab6abf1c1357ece89dce648554c33169c705e5e0d4da2363cb2b7ebde31dc9b8e2a24c61810f109a57c0b59415e5ddb95ee2ee568475c5abd8`,
+      },
+    };
+    const canvas = createCanvas();
+    axios
+      .get("https://api.neec.midas-cloud.xyz/api/certificados?filters[cert_uid][$eq]=" + this.$route.params.id + "&populate=Background&populate=signature", config)
+      .then((response) => {
+        let url_back = "https://api.neec.midas-cloud.xyz" + response.data.data[0].attributes.Background.data.attributes.url;
+        let background_w = response.data.data[0].attributes.Background.data.attributes.width;
+        let background_h = response.data.data[0].attributes.Background.data.attributes.height;
+        let name = response.data.data[0].attributes.participant;
+
+
+        // Create certificate
+        // Get background size
+        canvas.width = background_w;
+        canvas.height = background_h;
+        
+        const ctx = canvas.getContext("2d");
+
+        // Draw background
+        const background = new Image();
+        background.crossOrigin = "Anonymous";
+        let that = this;
+        background.onload = function () {
+          ctx.drawImage(background, 0, 0, background_w, background_h);
+          ctx.font = "bold 50px Arial";
+          ctx.fillStyle = "#000000";
+          ctx.textAlign = "center";
+          ctx.fillText(name, background_w / 2, background_h / 2 + 100);
+            that.certificateUrl = canvas.toDataURL();
+            that.initialized = true;
+        };
+        background.src = url_back;
+      })
+      .finally(() => {
+        //this.initialized = true;
+      });
+  },
+
+  //console.log("https://api.trello.com/1/cards/" + this.$route.params.id + "?attachments=true")
+  //axios.get("https://api.trello.com/1/cards/" + this.$route.params.id + "?attachments=true")
+  //    .then(response => {
+  //        console.log(response.data)
+  //        this.certificateUrl = response.data.attachments[0].url
+  //    }).catch(() => {
+  //        this.error = "Unable to find certificate"
+  //    }).finally(() => {
+  //        this.initialized = true
+  //    });
+  //}
 };
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
-    .certificate-app{
-    padding-top: 10vh;
-    padding-bottom: 7vh;
-    font-family: Roboto;
-    } 
-    .certificate-app img{
-	width: 80vw;
-	box-shadow: 0 0 7px 3px #888;
-    }
-   @media only screen and (max-width: 1080px){
-    .certificate-app {
-         padding-top: 80px;
-    }
-   }
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+
+.certificate-app {
+  padding-top: 10vh;
+  padding-bottom: 7vh;
+  font-family: Roboto;
+}
+
+.certificate-app img {
+  width: 80vw;
+  box-shadow: 0 0 7px 3px #888;
+}
+
+@media only screen and (max-width: 1080px) {
+  .certificate-app {
+    padding-top: 80px;
+  }
+}
 </style>
 <style>
 #page {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
-@media only screen and (max-width: 1080px){
-    .contacts-comp .row .col-md-4:first-child {
-         display: none;
-    }
-   }
+@media only screen and (max-width: 1080px) {
+  .contacts-comp .row .col-md-4:first-child {
+    display: none;
+  }
+}
+
 .contacts-comp {
-	position: unset;
-	margin-top: auto;
+  position: unset;
+  margin-top: auto;
 }
 </style>
--- site/src/pages/Projects.vue
+++ site/src/pages/Projects.vue
@@ -1,220 +1,222 @@
 <template>
-    <div class="projects-app">
+  <div class="projects-app">
+    <div v-if="initialized" class="projects-holder">
+      <div class="card" v-for="(card, card_id) in cardList" :key="card_id">
+        <div :style="{ padding: '3vw' }">
+          <div class="img-box">
+            <img v-if="card.image" :src="card.image" />
+          </div>
+          <h1 class="card-name">{{ card.name }}</h1>
+          <p class="card-desc" v-html="card.desc"></p>
+        </div>
 
-        <div  v-if="initialized" class="projects-holder">
-                <div class="card" v-for="(card,card_id) in cardList" :key="card_id">
-                    <div :style="{padding: '3vw'}">
-                        <div class="img-box">
-                            <img v-if="card.image" :src="card.image"/>
-                        </div>
-                        <h1 class="card-name">{{card.name}}</h1>
-                        <p class="card-desc" v-html="card.desc"></p>
-                    </div>
-                </div>
-            
-        </div>
-        <div v-else>
-            <PulseLoader :color="'#009DE0'"></PulseLoader>
-        </div>
+
+        
+      </div>
     </div>
+    <div v-else>
+      <PulseLoader :color="'#009DE0'"></PulseLoader>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import axios from 'axios';
-import PulseLoader from '../../node_modules/vue-spinner/src/PulseLoader.vue';
+import axios from "axios";
+import PulseLoader from "../../node_modules/vue-spinner/src/PulseLoader.vue";
 export default {
   name: "projects-page",
   data() {
-      return {
-        initialized: false,
-        cardList: [
-          
-        ]
-      }
+    return {
+      initialized: false,
+      cardList: [],
+    };
   },
-  components:{
-    PulseLoader
-},
-  methods:{
+  components: {
+    PulseLoader,
+  },
+  methods: {
     curateCardText(List) {
-        List.forEach(element => {
-            element.desc=this.linkify(element.desc)
-        });
+      List.forEach((element) => {
+        element.desc = this.linkify(element.desc);
+      });
     },
     linkify(inputText) {
-        //eslint-disable-next-line
-        const pattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-        let text = inputText.replace(pattern1, '<a class= "workpls" href="$1" target="_blank">$1</a>');
-        //eslint-disable-next-line
-        const pattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        text = text.replace(pattern2, '$1<a class= "workpls" href="http://$2" target="_blank">$2</a>');
-        
-        return text;
-    }
+      //eslint-disable-next-line
+      const pattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+      let text = inputText.replace(pattern1, '<a class= "workpls" href="$1" target="_blank">$1</a>');
+      //eslint-disable-next-line
+      const pattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+      text = text.replace(pattern2, '$1<a class= "workpls" href="http://$2" target="_blank">$2</a>');
+
+      return text;
+    },
   },
-  mounted(){
+  mounted() {
     this.curateCardText(this.cardList);
-  },created(){
-        axios.get("https://api.trello.com/1/lists/62ab561ec85b68533787cf3c/cards?attachments=true")
-        .then(response => {
-            response.data.forEach(element => {
-              this.cardList.push({name: element.name, image: element.attachments[0].url, desc :this.linkify(element.desc)})
-            });
-        }).finally(()=>{
-          this.initialized = true
-        })
-    }
+  },
+  created() {
+    axios
+      .get("https://api.trello.com/1/lists/62ab561ec85b68533787cf3c/cards?attachments=true")
+      .then((response) => {
+        response.data.forEach((element) => {
+          this.cardList.push({ name: element.name, image: element.attachments[0].url, desc: this.linkify(element.desc) });
+        });
+      })
+      .finally(() => {
+        this.initialized = true;
+      });
+  },
 };
 </script>
 
 <style scoped>
-
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
-.projects-app{
-    /* background: rgba(0,157,224,1); */
-/* //background: linear-gradient(193deg, rgba(255,255,255,1) 12%, rgba(0,157,224,1) 100%); */ 
-font-family: 'Roboto', sans-serif;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+.projects-app {
+  /* background: rgba(0,157,224,1); */
+  /* //background: linear-gradient(193deg, rgba(255,255,255,1) 12%, rgba(0,157,224,1) 100%); */
+  font-family: "Roboto", sans-serif;
 }
-.workpls{
-    color: #ea0000;
+.workpls {
+  color: #ea0000;
 }
-.projects-holder{
-    padding-bottom: 190px;
-    padding-top: 100px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100%;
-    justify-content: space-evenly;
+.projects-holder {
+  padding-bottom: 190px;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: space-evenly;
 }
 
 .card {
-    border-radius: 32px;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;    
-    backdrop-filter: blur(15px);
-    text-align: center;
-    margin-top: 2vw;
-    margin-bottom: 2vw;
-    width: 30%;
-    color: #ffffff;
-    background-color: #303633;
+  border-radius: 32px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  backdrop-filter: blur(15px);
+  text-align: center;
+  margin-top: 2vw;
+  margin-bottom: 2vw;
+  width: 30%;
+  color: #ffffff;
+  background-color: #303633;
 }
 
-.card .img-box{
-    height: 45vh;
-    align-items: center;
-    display: flex;
-    justify-content: center;
+.card .img-box {
+  height: 45vh;
+  align-items: center;
+  display: flex;
+  justify-content: center;
 }
 
-.card img{
-    width: auto;
-    border-radius: 12px;
-    height: auto;
-    object-fit: contain;
-    max-width: 100%;
-    max-height: 100%;
+.card img {
+  width: auto;
+  border-radius: 12px;
+  height: auto;
+  object-fit: contain;
+  max-width: 100%;
+  max-height: 100%;
 }
 
-.card-name{
-    font-family: 'Michroma', sans-serif;
+.card-name {
+  font-family: "Michroma", sans-serif;
+  color: darkred;
+  font-size: xxx-large;
 }
-.card-desc{    
-    font-size: 1.2rem;
-    opacity: 0.8;
-    white-space: break-spaces;
+.card-desc {
+  font-size: 1.2rem;
+  opacity: 0.8;
+  white-space: break-spaces;
 }
-
 
 @media screen and (max-width: 1200px) {
-    .team-flex {
-        padding-left: 12px;
-        margin-right: 5vw;
-        margin-left: 5vw;
-    }
+  .team-flex {
+    padding-left: 12px;
+    margin-right: 5vw;
+    margin-left: 5vw;
+  }
 }
 @media screen and (max-width: 800px) {
-    .description-title{
-        font-size: 40px;
-    }
-    .description-text{
-        font-size: 18px;
-        width: 320px;
-        padding-right: 40px;
-        line-height: 28px;
-    }
-    .description-projects img{
-        width: 475px;
-        top: 200px;
-    }
-    
-    .team-flex {
-        padding-left: 12px;
-        margin-right: 5vw;
-        margin-left: 5vw;
-    }
-    .team-flex img{
-        height: 140px;
-        width: 140px;
-        margin-top: 30px;
-        flex: 0 0 50%;
-        margin-right: 10px;
-    }
-    .member-name{
-        font-size: 17px;
-        margin-left:13%;
-    }
-    .icon-team-member {
-        height: 40px !important;
-        width: 40px !important;
-        margin-top: 20px !important;
-        margin-right: 0px !important;
-    }
+  .description-title {
+    font-size: 40px;
+  }
+  .description-text {
+    font-size: 18px;
+    width: 320px;
+    padding-right: 40px;
+    line-height: 28px;
+  }
+  .description-projects img {
+    width: 475px;
+    top: 200px;
+  }
+
+  .team-flex {
+    padding-left: 12px;
+    margin-right: 5vw;
+    margin-left: 5vw;
+  }
+  .team-flex img {
+    height: 140px;
+    width: 140px;
+    margin-top: 30px;
+    flex: 0 0 50%;
+    margin-right: 10px;
+  }
+  .member-name {
+    font-size: 17px;
+    margin-left: 13%;
+  }
+  .icon-team-member {
+    height: 40px !important;
+    width: 40px !important;
+    margin-top: 20px !important;
+    margin-right: 0px !important;
+  }
 }
 @media screen and (max-width: 400px) {
-    .description-projects{
-        height: 200px;
-        background-color:white;
-        position: relative;
-    }
-    .description-title{
-        font-size: 30px;
-        padding-top: 10px;
-    }
-    .description-text{
-        font-size: 16px;
-        width: 250px;
-        padding-right: 30px;
-        padding-left: 20px;
-        line-height: 24px;
-    }
-    .description-projects img{
-        width: 475px;
-        top: 150px;
-    }
-    .team{
-        padding-top: 20px;
-    }
-    
-    .team-flex {
-        margin-right: 2vw;
-        margin-left: 2vw;
-    }
-    .team-flex img{
-        height: 120px;
-        width: 120px;
-        margin-top: 5px;
-        flex: 0 0 50%; margin-right: 10px;
-    }
-    .member-name{
-        margin-left:6%;
-    }
+  .description-projects {
+    height: 200px;
+    background-color: white;
+    position: relative;
+  }
+  .description-title {
+    font-size: 30px;
+    padding-top: 10px;
+  }
+  .description-text {
+    font-size: 16px;
+    width: 250px;
+    padding-right: 30px;
+    padding-left: 20px;
+    line-height: 24px;
+  }
+  .description-projects img {
+    width: 475px;
+    top: 150px;
+  }
+  .team {
+    padding-top: 20px;
+  }
+
+  .team-flex {
+    margin-right: 2vw;
+    margin-left: 2vw;
+  }
+  .team-flex img {
+    height: 120px;
+    width: 120px;
+    margin-top: 5px;
+    flex: 0 0 50%;
+    margin-right: 10px;
+  }
+  .member-name {
+    margin-left: 6%;
+  }
 }
 </style>
 
-<style> 
+<style>
 a {
-    overflow-wrap: anywhere;
+  overflow-wrap: anywhere;
 }
 </style>
